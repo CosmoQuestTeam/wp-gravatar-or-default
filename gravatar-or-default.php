ae817f94ca497338ba1d8d6693f2b20903c7be8d
Ryan Owens <RyanOwens@linux.com>
2016-10-14 15:37:53 -0500

    Get the value properly and increased URL length

--- gravatar-or-default.php
+++ gravatar-or-default.php
@@ -25,7 +25,7 @@ $charset_collate = $wpdb->get_charset_collate();
 $sql = "CREATE TABLE $table_name (
   id mediumint(9) NOT NULL AUTO_INCREMENT,
   user_id mediumint(9) NOT NULL UNIQUE,
-  avatar_url varchar,
+  avatar_url varchar(150),
   PRIMARY KEY  (id)
 ) $charset_collate;";
 
