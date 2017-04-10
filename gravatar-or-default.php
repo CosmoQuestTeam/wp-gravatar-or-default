e1315e457e7d9be05a7ba80b5ef3b7a2b5266d35
Ryan Owens <ryanvader82@gmail.com>
2017-04-10 11:55:16 -0500

    Update gravatar-or-default.php

--- gravatar-or-default.php
+++ gravatar-or-default.php
@@ -33,7 +33,7 @@ $sql = "CREATE TABLE $table_name (
 
  require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
  dbDelta( $sql );
- switch_to_blog ($blog_id);
+ restore_current_blog();
 }
 
 function gravatar_or_default_get_avatar($avatar, $id_or_email, $size, $default, $alt)
@@ -41,7 +41,6 @@ function gravatar_or_default_get_avatar($avatar, $id_or_email, $size, $default, 
   $user = false;
   $avatar = "";
   global $wpdb;
- $blog_id = get_current_blog_id();
   switch_to_blog (1);
     if ( is_numeric( $id_or_email ) ) {
 
@@ -70,6 +69,6 @@ function gravatar_or_default_get_avatar($avatar, $id_or_email, $size, $default, 
         $avatar = "<img alt='{$alt}' src='{$default}' class='avatar avatar-{$size} photo' height='{$size}' width='{$size}' />";
       }
     }
-    switch_to_blog ($blog_id);
+    restore_current_blog();
     return $avatar;
 }
